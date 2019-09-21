b5e57ecac237b15886ef7d76128d36c62d428e21
wangpin <1151749070@qq.com>
2019-09-21 13:03:45 +0800

    update nacos address to 120.79.160.65

--- order-server/src/main/java/cn/wpin/mall/order/OrderApplication.java
+++ order-server/src/main/java/cn/wpin/mall/order/OrderApplication.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author wangpin
@@ -11,6 +12,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 @MapperScan("cn.wpin.mall.order")
+@EnableFeignClients(value = "cn.wpin.mall.client")
 public class OrderApplication {
 
     public static void main(String[] args) {
